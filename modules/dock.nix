6c2ce06f34b74e2e3784d0c34c275079e2ca9b35
z0al <12673605+z0al@users.noreply.github.com>
2024-12-22 21:36:52 +0000

    feat: add `dock.autoHide`

--- modules/dock.nix
+++ modules/dock.nix
@@ -7,23 +7,57 @@ in
 {
   options.defaults.dock = with lib; {
     position = mkOption {
-      description = ''
-        Set the Dock position
-      '';
-
       type = types.nullOr (types.enum [
         "bottom"
         "left"
         "right"
       ]);
-
+      description = ''
+        Position of the dock on screen
+      '';
       default = null;
+    };
+
+    autoHide = {
+      enable = mkOption {
+        type = types.nullOr types.bool;
+        description = ''
+          Whether to automatically hide and show the dock
+        '';
+        default = null;
+      };
+
+      delay = mkOption {
+        type = types.nullOr types.float;
+        description = ''
+          Sets the speed of the auto-hide delay
+        '';
+        default = null;
+      };
+
+      animationDelay = mkOption {
+        type = types.nullOr types.float;
+        description = ''
+          Sets the speed of the animation when hiding/showing the Dock
+        '';
+        default = null;
+      };
     };
   };
 
   config.defaults._impl = {
     "com.apple.dock" = {
+      # Position
       orientation = cfg.position;
+
+      # Auto hide
+      autohide = cfg.autoHide.enable;
+      autohide-delay =
+        if cfg.autoHide.enable
+        then cfg.autoHide.delay else null;
+      autohide-time-modifier =
+        if cfg.autoHide.enable
+        then cfg.autoHide.animationDelay else null;
     };
   };
 }
--- tests/dock.nix
+++ tests/dock.nix
@@ -7,12 +7,33 @@ in
 {
   defaults.dock = {
     position = "left";
+
+    autoHide = {
+      enable = true;
+      delay = 0.5;
+      animationDelay = 0.5;
+    };
   };
 
   assertions = [
     {
       message = "expects dock position to equal 'left'";
       assertion = pref."com.apple.dock".orientation == "left";
+    }
+
+    {
+      message = "expects dock auto-hide to be enabled";
+      assertion = pref."com.apple.dock".autohide == true;
+    }
+
+    {
+      message = "expects dock auto-hide delay to equal 0.5";
+      assertion = pref."com.apple.dock".autohide-delay == 0.5;
+    }
+
+    {
+      message = "expects dock auto-hide time modifier to equal 0.5";
+      assertion = pref."com.apple.dock".autohide-time-modifier == 0.5;
     }
   ];
 }
